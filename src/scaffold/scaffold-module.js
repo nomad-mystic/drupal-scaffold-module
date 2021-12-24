14749b5785f724db4be97e47c3c027432273128b
Keith Murphy <keith@flickerbox.com>
2021-12-23 18:36:03 -0800

    kpm: Refactor renaming files into a node function

--- src/scaffold/scaffold-module.js
+++ src/scaffold/scaffold-module.js
@@ -1,15 +1,12 @@
 // Community
-const fs = require('fs');
+
 const fse = require('fs-extra');
-const glob = require('glob');
-const path = require('path');
 
 // Internal
 const { whereAmI, isDrupalInstall, getModulesFolderPath } = require('../utils/path-utils');
+const { renameBaseFiles } = require('../utils/rename-base-files');
 
 const scaffoldModule = function(answers) {
-  // console.log(answers);
-
   // Absolute path of the custom folder
   const customPath = getModulesFolderPath();
 
@@ -23,31 +20,10 @@ const scaffoldModule = function(answers) {
   // Copy our files over to the modules folder
   fse.copySync('./scaffolding', modulePath, {});
 
-  // Grab our YML files paths
-  glob(`${customPath}/${machineName}/**/*.yml`, {}, function (err, files) {
-    // Check for errors first
-    if (err) {
-      throw err;
-    }
 
-    // Sanity check we have a files array
-    if (!files || typeof files === 'undefined' || files.length <= 0) {
-      console.error('There was an issue with grabbing our YML files. Please reach-out to the author of this package.');
-    }
-
-    // For each of the YML files rename them based on user input
-    for (let file = 0; file < files.length; file++) {
-
-      // Sanity check
-      if (files[file] && typeof files[file] !== 'undefined') {
-        let fileName = path.basename(files[file]);
-        let afterFirstDot = fileName.match(/\.(.*)/)[0]; // We want [0]
-
-        // Change the file names
-        fs.renameSync(`${modulePath}/${fileName}`, `${modulePath}/${machineName}${afterFirstDot}`);
-      }
-    }
-  });
+  // Rename the YML and module file based on user input
+  renameBaseFiles(modulePath, machineName, '.yml');
+  renameBaseFiles(modulePath, machineName, '.module');
 };
 
 module.exports = {
---
+++ src/utils/rename-base-files.js
@@ -0,0 +1,48 @@
+// Community
+const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
+
+/**
+ * @description Renames the .yml and module file based on user input
+ *
+ * @param {string} modulePath
+ * @param {string} machineName
+ * @param {string} fileType
+ * @return void
+ */
+const renameBaseFiles = function(modulePath, machineName, fileType) {
+  // Grab our YML files paths
+  glob(`${modulePath}/**/*${fileType}`, {}, function (err, files) {
+    // Check for errors first
+    if (err) {
+      throw err;
+    }
+
+    // Sanity check we have a files array
+    if (!files || typeof files === 'undefined' || files.length <= 0) {
+      console.error(`There was an issue with grabbing our ${fileType} files. Please reach-out to the author of this package.`);
+    }
+
+    // For each of the YML files rename them based on user input
+    for (let file = 0; file < files.length; file++) {
+
+      // Sanity check
+      if (files[file] && typeof files[file] !== 'undefined') {
+        let fileName = path.basename(files[file]);
+        let afterFirstDot = fileName.match(/\.(.*)/)[0]; // We want [0]
+
+        // Change the file names
+        fs.renameSync(`${modulePath}/${fileName}`, `${modulePath}/${machineName}${afterFirstDot}`);
+      }
+    }
+  });
+};
+
+module.exports = {
+  renameBaseFiles
+};
+
+
+
+
